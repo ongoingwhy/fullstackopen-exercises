54a59184412238fa902f7ec3e6d0d225bb334032
ongoingwhy <42935285+ongoingwhy@users.noreply.github.com>
2019-10-17 13:41:44 +0800

    finished phonebook step 5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 
+const Filter =({onchange, filter}) => <input value={filter} onChange={onchange}/>
 
-const ShowForm = ({onsubmit, onchange, newname}) => {
+const InputForm = ({onsubmit, onchangeName, onchangeNum, onchangeFilter, newName, newNum, filter}) => {
     return (
         <form onSubmit={onsubmit}>
             <div>
-            name: <input value={newname} onChange={onchange}/>
+              name: <input value={newName} onChange={onchangeName}/>
+            </div>
+            <div>
+              number: <input value={newNum} onChange={onchangeNum}/>
             </div>
             <div>
                 <button type="submit">add</button> 
@@ -14,22 +18,37 @@ const ShowForm = ({onsubmit, onchange, newname}) => {
     );
 }
 
-const ShowContacts = ({contacts}) => {
-    const rows = () => contacts.map( (x) => <li key={x.name}>{x.name}</li> );
+const Contact = ({name, number}) => <li key={name}>{name} {number}</li>
+
+const Contacts = ({contacts, filter}) => {
+    const rows = () => {
+      let display = [...contacts]
+      if (filter !== '') {
+        display = contacts.filter( (contact) => contact.name.toUpperCase().indexOf(filter.toUpperCase()) >= 0)
+      }
+
+      return (
+        display.map( (contact) => <Contact name={contact.name} number={contact.number} />)
+      );
+    }
 
     return (
-        <ul>
-            {rows()}
-        </ul>
+      <ul>
+          {rows()}
+      </ul>
     );
 }
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
-
   const [ newName, setNewName ] = useState('');
+  const [ newNum, setNewNum ] = useState('');
+  const [ searchFilter, setFilter ] = useState('');
 
   const addContact = (ev) => {
     ev.preventDefault();
@@ -37,22 +56,30 @@ const App = () => {
     if (persons.findIndex(x => x.name === newName) >= 0)
       alert(`${newName} is already added to the phonebook`);
     else {
-      let newPersons = persons.concat({name: newName});
+      let newPersons = persons.concat({name: newName, number: newNum});
       setPersons(newPersons);
       setNewName('');
+      setNewNum('');
     }
   }
 
-  const updateNewName = (ev) => {
-      setNewName(ev.target.value);
-  }
+  const updateNewName = (ev) => setNewName(ev.target.value);
+
+  const updateNewNum = (ev) => setNewNum(ev.target.value);
+
+  const updateFilter = (ev) => setFilter(ev.target.value);
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <ShowForm onsubmit={addContact} onchange={updateNewName} newname={newName}/>
+      <Filter onchange={updateFilter} filter={searchFilter}/>
+
+      <h2>add a new</h2>
+      <InputForm onsubmit={addContact} onchangeName={updateNewName} newName={newName}
+      onchangeNum={updateNewNum} newNum={newNum} />
+
       <h2>Numbers</h2>
-      <ShowContacts contacts={persons} />
+      <Contacts contacts={persons} filter={searchFilter} />
     </div>
   )
 }
